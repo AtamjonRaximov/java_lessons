3d1440bb63ad2be72f7af0e7c9b5f6b851928f38
AtamjonRaximov <raximovatam999@gmail.com>
2025-07-21 10:09:59 +0500

    add .

--- app.java
+++ app.java
@@ -3,23 +3,41 @@ import java.util.Scanner;
 public class app {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("enter text: ");
-        String s = scanner.nextLine();
-        int counter =0;
+
+        System.out.print("enter n number: ");
+        int n = scanner.nextInt();
+
+
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("enter arr's " + i + " value: ");
+            arr[i] += scanner.nextInt();
+
+        }
 
 
 
-        for (int i = 0; i<s.length();i++ ){
-            if(s.substring(i,i+1).equalsIgnoreCase("b") && s.substring(i+2,i+3).equalsIgnoreCase("b")){
-                System.out.println(true);
-                counter++;
-                break;
+
+        for (int i = 0; i < n; i++) {
+            boolean isduplicate = false;
+
+            for (int j = 0; j < i; j++) {
+                if(arr[i]==arr[j]){
+                    isduplicate=true;
+                    break;
+                }
+
             }
+            if(!isduplicate){
+                System.out.print(arr[i]+" ");
+            }
+
+
         }
 
-        if (counter==0){
-            System.out.println(false);
-        }
+
     }
 
 }
